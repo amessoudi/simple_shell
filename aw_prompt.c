1eb5e008882e4fb6a684ec30f852d179f2fbf4ca
Clear008 <wissal-exo@hotmail.com>
2023-11-06 05:14:01 -0800

    aw_prompt.c

---
+++ aw_prompt.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+/**
+ *main - check the code
+ *@argv: array of command line argument
+ *@argc: number of command line argument
+ * Return: Always 0.
+ */
+
+
+int main(int argc, char *argv[])
+{
+    char *aw_line = NULL;
+    size_t aw_len = 0;
+    ssize_t aw_nread;
+    (void)argc;
+    (void)argv;
+
+    while (1)
+{
+if (isatty(STDIN_FILENO) == 1)
+write(STDOUT_FILENO, "aw$ ", 4);
+        aw_nread = getline(&aw_line, &aw_len, stdin);
+
+        if (aw_nread == -1)
+        {
+            free(aw_line);
+            write(STDOUT_FILENO, "\n", 1);
+            break;
+        }
+        else
+        {
+            write(STDOUT_FILENO, aw_line, aw_nread);
+        }
+
+    }
+
+    exit(EXIT_SUCCESS);
+}
