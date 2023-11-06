5c244651ae9864f9bedb28c7057f0477b95a5ddc
amessoudi <amessoudi150@gmail.com>
2023-11-06 22:51:50 +0100

    Fixed error message

--- aw_prompt.c
+++ aw_prompt.c
@@ -8,69 +8,70 @@
  */
 int main(int argc, char *argv[])
 {
-    char *aw_line = NULL;
-    size_t aw_len = 0;
-    ssize_t aw_nread;
-    char *aw_token;
-    char *aw_args[64];
-    char *aw_full_path;
-    int aw_i;
+	char *aw_line = NULL;
+	size_t aw_len = 0;
+	ssize_t aw_nread;
+	char *aw_token;
+	char *aw_args[64];
+	char *aw_full_path;
+	int aw_i;
 	int aw_last_status = 0;
 
-    (void)argc;
-    (void)argv;
+	(void)argc;
+	(void)argv;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO) == 1)
-        {
-            write(STDOUT_FILENO, "aw$ ", 4);
-        }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO) == 1)
+		{
+			write(STDOUT_FILENO, "aw$ ", 4);
+		}
 
-        aw_nread = getline(&aw_line, &aw_len, stdin);
-        if (aw_nread == -1)
-        {
-            free(aw_line);
-            if (isatty(STDIN_FILENO) == 1)
-            {
-                write(STDOUT_FILENO, "\n", 1);
-            }
-            break;
-        }
+		aw_nread = getline(&aw_line, &aw_len, stdin);
+		if (aw_nread == -1)
+		{
+			free(aw_line);
+			if (isatty(STDIN_FILENO) == 1)
+			{
+				write(STDOUT_FILENO, "\n", 1);
+			}
+			break;
+		}
 
-        aw_i = 0;
-        aw_token = strtok(aw_line, " \t\r\n\a");
-        while (aw_token != NULL)
-        {
-            aw_args[aw_i++] = aw_token;
-            if (aw_i >= 63)
-            {
-                write(STDOUT_FILENO, "Error: too many arguments\n", 26);
-                break;
-            }
-            aw_token = strtok(NULL, " \t\r\n\a");
-        }
-        aw_args[aw_i] = NULL;
+		aw_i = 0;
+		aw_token = strtok(aw_line, " \t\r\n\a");
+		while (aw_token != NULL)
+		{
+			aw_args[aw_i++] = aw_token;
+			if (aw_i >= 63)
+			{
+				write(STDOUT_FILENO, "Error: too many arguments\n", 26);
+				break;
+			}
+			aw_token = strtok(NULL, " \t\r\n\a");
+		}
+		aw_args[aw_i] = NULL;
 
-        if (aw_args[0])
-        {
-            aw_full_path = aw_find_command_in_path(aw_args[0]);
-            if (aw_full_path != NULL)
-            {
-                aw_execute_command(aw_full_path, aw_args);
-                free(aw_full_path);
-            }
-            else
-            {
-                write(STDOUT_FILENO, aw_args[0], strlen(aw_args[0]));
-                write(STDOUT_FILENO, ": command not found\n", 20);
+		if (aw_args[0])
+		{
+			aw_full_path = aw_find_command_in_path(aw_args[0]);
+			if (aw_full_path != NULL)
+			{
+				aw_execute_command(aw_full_path, aw_args);
+				free(aw_full_path);
+			}
+			else
+			{
+				char error_message[50];
+				snprintf(error_message, sizeof(error_message), "%s: %s: not found\n", argv[0], aw_args[0]);
+				write(STDERR_FILENO, error_message, strlen(error_message));
 				aw_last_status = 127;
-            }
-        }
-        free(aw_line);
-        aw_line = NULL;
-        aw_len = 0;
-    }
+			}
+		}
+		free(aw_line);
+		aw_line = NULL;
+		aw_len = 0;
+	}
 
-    exit(aw_last_status);
+	exit(aw_last_status);
 }
