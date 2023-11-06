282ba8aaff963aa9a08850c1322984bfcb6b8877
amessoudi <amessoudi150@gmail.com>
2023-11-06 16:27:36 +0100

    Fixed for Betty

--- aw_prompt.c
+++ aw_prompt.c
@@ -9,30 +9,30 @@
 
 int main(int argc, char *argv[])
 {
-    char *aw_line = NULL;
-    size_t aw_len = 0;
-    ssize_t aw_nread;
-    (void)argc;
-    (void)argv;
+	char *aw_line = NULL;
+	size_t aw_len = 0;
+	ssize_t aw_nread;
+	(void)argc;
+	(void)argv;
 
-    while (1)
-{
-if (isatty(STDIN_FILENO) == 1)
-write(STDOUT_FILENO, "aw$ ", 4);
-        aw_nread = getline(&aw_line, &aw_len, stdin);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO) == 1)
+		write(STDOUT_FILENO, "aw$ ", 4);
+		aw_nread = getline(&aw_line, &aw_len, stdin);
 
-        if (aw_nread == -1)
-        {
-            free(aw_line);
-            write(STDOUT_FILENO, "\n", 1);
-            break;
-        }
-        else
-        {
-            write(STDOUT_FILENO, aw_line, aw_nread);
-        }
+		if (aw_nread == -1)
+		{
+			free(aw_line);
+			write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+		else
+		{
+			write(STDOUT_FILENO, aw_line, aw_nread);
+		}
 
-    }
+	}
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
